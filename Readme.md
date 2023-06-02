5986d9f66de63a4472ff708646d9bed4c44ba665
Rishabh611 <44869319+Rishabh611@users.noreply.github.com>
2023-06-02 23:04:37 +0530

    mongo

--- Readme.md
+++ Readme.md
@@ -1,3 +1,11 @@
 # deploying app to internet
 
 -   Will be deploying to render.
+
+# Saving Data to MongoDB
+
+-   Debugging: Read Later
+
+-   MongoDB - Document Database - NoSQL - MongoDB stores data records as documents (specifically BSON documents) which are gathered together in collections. A database stores one or more collections of documents. - Collections
+    MongoDB stores documents in collections. Collections are analogous to tables in relational databases. - MongoDB stores data records as BSON documents. BSON is a binary representation of JSON documents, though it contains more data types than JSON. - username rishabsinghrock password YcsVMoNUihHkfBGX - connection string: mongodb+srv://rishabsinghrock:<password>@cluster0.obgbsat.mongodb.net/?retryWrites=true&w=majority - We could use the database directly from our JavaScript code with the official MongoDB Node.js driver library, but it is quite cumbersome to use. We will instead use the **Mongoose library** that offers a higher-level API. - `npm install mongoose` - `const mongoose = require('mongoose')` <= require mongoose - `const url =
+`mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`` <+= mongo db connection url -
--- index.js
+++ index.js
@@ -1,74 +1,93 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-app.use(express.json());
-const cors = require("cors");
-app.use(express.static("build"));
-app.use(cors());
-let notes = [
-	{
-		id: 1,
-		content: "HTML is easy",
-		important: true,
-	},
-	{
-		id: 2,
-		content: "Browser can execute only JavaScript",
-		important: false,
-	},
-	{
-		id: 3,
-		content: "GET and POST are the most important methods of HTTP protocol",
-		important: true,
-	},
-];
+const Note = require("./models/note");
+const requestLogger = (request, response, next) => {
+	console.log("Method:", request.method);
+	console.log("Path:  ", request.path);
+	console.log("Body:  ", request.body);
+	console.log("---");
+	next();
+};
 
-app.get("/", (request, response) => {
-	response.send("<h1>Hello World!</h1>");
-});
+app.use(express.static("build"));
+app.use(express.json());
+app.use(requestLogger);
 
 app.get("/api/notes", (request, response) => {
-	response.json(notes);
+	Note.find({}).then((notes) => {
+		response.json(notes);
+	});
 });
 
-app.get("/api/notes/:id", (request, response) => {
-	const id = Number(request.params.id);
-	console.log(id);
-	const note = notes.find((note) => note.id === id);
-	if (note) {
-		response.json(note);
-	} else {
-		response.status(404).end();
-	}
-
-	response.json(note);
-});
-app.delete("/api/notes/:id", (request, response) => {
-	const id = Number(request.params.id);
-	notes = notes.filter((note) => note.id !== id);
-
-	response.status(204).end();
-});
-const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-	return maxId + 1;
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({error: "unknown endpoint"});
 };
-app.post("/api/notes", (request, response) => {
-	const body = request.body;
-	if (!body.content) {
-		return response.status(400).json({
-			error: "content missing",
-		});
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+	console.log(erros.message);
+	if (error.name === "CastError") {
+		return response.status(404).send({error: "Malformatted id"});
+	} else if (error.name === "ValidationError") {
+		return response.status(404).json({error: error.message});
 	}
-	const note = {
+	next(error);
+};
+
+app.use(errorHandler);
+
+app.post("/api/notes", (request, response, next) => {
+	const body = request.body;
+
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
-		id: generateId(),
-	};
-	notes = notes.concat(note);
+	});
 
-	response.json(note);
+	note.save()
+		.then((savedNote) => {
+			response.json(savedNote);
+		})
+		.catch((error) => next(error));
 });
-const PORT = process.env.PORT || 3001;
+
+app.get("/api/notes/:id", (request, response, next) => {
+	Note.findById(request.params.id)
+		.then((note) => {
+			if (note) {
+				response.json(note);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
+});
+
+app.delete("/api/notes/:id", (request, response, next) => {
+	Note.findByIdAndDelete(request.params.id)
+		.then((result) => {
+			reponse.status(204).end();
+		})
+		.catch((error) => next(error));
+});
+
+app.put("/api/notes/:id", (request, response, next) => {
+	const body = request.body;
+	const {content, important} = request.body;
+	Note.findByIdAndUpdate(
+		request.params.id,
+		{content, important},
+		{new: true, runValidators: true, context: "query"}
+	)
+		.then((updateNote) => {
+			response.json(updateNote);
+		})
+		.catch((error) => next(error));
+});
+
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
